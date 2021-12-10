2b032f02f4d66813ffe07a47701918820e4ac3d6
rk2-182 <ricardoplus@live.cl>
2021-12-10 16:03:14 -0300

    se corrigen detalles menores

--- teclado.py
+++ teclado.py
@@ -1,9 +1,14 @@
 import pyautogui
 import time
 
+#https://www.youtube.com/watch?v=N2-v0scoTVo&list=PLrMKroDxWn7JEtvM8Kvem46_u1vlBSHxY&index=3&t
+
 pyautogui.hotkey("win","r")
 #pyautogui.press("enter")
 posicion=pyautogui.locateCenterOnScreen('img/abrir.png',confidence=.8)
 pyautogui.moveTo(posicion, duration=3)
 pyautogui.moveRel(80,0,duration=.3)
-pyautogui.click(clicks=2,interval=.1)
+pyautogui.click(clicks=1,interval=.1)
+pyautogui.press("backspace")
+pyautogui.write("calc")
+pyautogui.press("enter")
